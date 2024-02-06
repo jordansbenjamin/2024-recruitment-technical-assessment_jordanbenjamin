566f9e4c8abe94e6e5a87ea2e9e4ef2e988c8b11
jordansbenjamin <14327@coderacademy.edu.au>
2024-02-07 10:01:59 +1100

    feat(main): implemented heading

--- frontend/devsoc-frontend/src/layout/Main.tsx
+++ frontend/devsoc-frontend/src/layout/Main.tsx
@@ -1,3 +1,13 @@
 export default function Main() {
-	return <div className="bg-slate-50 w-full">Main</div>;
+	return (
+		<main className="bg-slate-50 w-full flex flex-col items-center">
+			<section className="w-[60rem] mt-6">
+				<h2 className="text-gray-600 mb-[-.7rem]">DevSoc presents</h2>
+				<h1 className="text-[5.5rem] text-blue-500 font-bold">unilectives</h1>
+				<p className="font-bold mt-[-.6rem] text-lg">Your one-stop shop for UNSW course and elective reviews.</p>
+			</section>
+			<section></section>
+			<section></section>
+		</main>
+	);
 }
