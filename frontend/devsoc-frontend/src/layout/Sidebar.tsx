b4144a5383975817beafab65be04ed5d9d5968eb
jordansbenjamin <14327@coderacademy.edu.au>
2024-02-07 08:59:10 +1100

    fix(svg): imported svg as url

--- frontend/devsoc-frontend/src/layout/Sidebar.tsx
+++ frontend/devsoc-frontend/src/layout/Sidebar.tsx
@@ -1,3 +1,26 @@
+import UnilectivesLogo from "../assets/unilectives.svg";
+
+// type TUnilectiveLogo = HTMLElement & SVGAElement;
+
+// const unilectiveLogo: TUnilectiveLogo = unilectivesSvg;
+
 export default function Sidebar() {
-	return <aside className="bg-slate-100 w-[6.5rem]">Sidebar</aside>;
+	return (
+		<aside className="bg-slate-100 w-[6.5rem] flex flex-col">
+			<img src={UnilectivesLogo} alt="" />
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				strokeWidth={1.5}
+				stroke="currentColor"
+				className="w-6 h-6">
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+				/>
+			</svg>
+		</aside>
+	);
 }
